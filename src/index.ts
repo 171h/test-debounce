59f0cfcdd24d0e66e59624e60473a1bee1d30e0f
H171 <1091088708@qq.com>
2023-07-03 22:35:43 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-export const one = 1
-export const two = 2
+/* eslint-disable no-console */
+import { debounce } from 'perfect-debounce'
+import timeSpan from 'time-span'
+
+const end = timeSpan()
+
+let count = 0
+
+async function testsync(i: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('async start...', count, i)
+      resolve(true)
+    }, 500)
+  })
+}
+
+const debounced = debounce((i: number) => {
+  count++
+  testsync(i)
+}, 25, { diff: true })
+
+for (const i of [1, 2, 3, 1, 2, 3, 4, 1, 4, 5, 6, 1, 3])
+  debounced(i)
