0a9c2aa8c8b08e1f35adaf478d9773cf18afee8e
H171 <1091088708@qq.com>
2023-08-11 14:57:19 +0800

    chore: update

---
+++ pages/index_bak.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const online = useOnline()
+</script>
+
+<template>
+  <div>
+    <Logos mb-6 />
+    <Suspense>
+      <ClientOnly>
+        <PageView v-if="online" />
+        <div v-else text-gray:80>
+          You're offline
+        </div>
+      </ClientOnly>
+      <template #fallback>
+        <div italic op50>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    <InputEntry />
+    <hr>
+    <TestAccordion />
+    <TestDefineShortcuts />
+    <TestAlert />
+    <TestAvatar />
+    <TestBadge />
+    <TestButton />
+    <TestDropdown />
+    <TestIcon />
+    <TestKbd />
+    <TestLink />
+    <TestInput />
+    <TestTextarea />
+    <TestSelect />
+    <TestSelectMenu />
+    <TestCheckbox />
+  </div>
+</template>
+
+<style>
+h1 {
+  text-align: left;
+  font-weight: bold;
+  margin-top: 1rem;
+}
+h1::before {
+  content: '🧪';
+  margin-right: 0.5rem;
+}
+</style>
